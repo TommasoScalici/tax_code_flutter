9d4e1ddf13a8ff70ed4bea59a5bbe3cb531c1af3
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2024-10-23 09:20:11 +0200

    Removed Wear OS source files from main project, Wear OS app will be moved in separated project

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:material_symbols_icons/symbols.dart';
 import 'package:provider/provider.dart';
 
+import '../models/contact.dart';
 import '../providers/app_state.dart';
-import '../wear_os/screens/home_view_watch.dart';
-import '../widgets/home_view_phone.dart';
+import '../widgets/contacts_list.dart';
+import '../widgets/info_modal.dart';
+
+import 'form_page.dart';
+import 'profile_screen.dart';
 
 final class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,13 +31,83 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth < 300) {
-        return HomeViewWatch();
-      } else {
-        return HomeViewPhone();
-      }
-    });
+    return Consumer<AppState>(
+      builder: (BuildContext context, AppState value, Widget? child) {
+        final currentUser = FirebaseAuth.instance.currentUser;
+
+        return Scaffold(
+          appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(AppLocalizations.of(context)!.appTitle),
+              actions: [
+                IconButton(
+                    onPressed: () async {
+                      await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfileScreen()));
+                    },
+                    icon: currentUser != null && currentUser.photoURL != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image.network(currentUser.photoURL!),
+                          )
+                        : Icon(Symbols.account_circle_filled)),
+                IconButton(
+                  icon: Icon(value.theme == 'dark'
+                      ? Icons.light_mode_sharp
+                      : Icons.mode_night_sharp),
+                  onPressed: () {
+                    value.toggleTheme();
+                  },
+                ),
+                PopupMenuButton(
+                  child: const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Icon(Icons.more_vert)),
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Row(
+                        children: [
+                          const Icon(Icons.info),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(AppLocalizations.of(context)!.info),
+                          )
+                        ],
+                      ),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) => const InfoModal(),
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+          body: const ContactsListPage(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              final contact = await Navigator.push<Contact>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const FormPage(contact: null),
+                  ));
+
+              var currentFocus = FocusManager.instance.primaryFocus;
+              if (currentFocus != null) currentFocus.unfocus();
+
+              await Future.delayed(const Duration(milliseconds: 500));
+
+              if (contact != null) {
+                value.addContact(contact);
+                value.saveContacts();
+              }
+            },
+            tooltip: AppLocalizations.of(context)!.newItem,
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
+    );
   }
 }
